8e2917a54a70a811572d12c6f4cb4e36761065ad
Pieter <oosthuizenp@me.com>
2026-06-02 00:20:54 +0200

    build: upgrade webpack and build configuration
    Update build tooling and assets to improve site SEO and asset management.
    
    - Upgrade webpack from v4 to v5 and webpack-cli to v5
    - Replace node-sass with sass
    - Update webpack configuration to use asset modules instead of file/url loaders
    - Update Gemfile to allow a broader range of Ruby 3.2 versions
    - Improve SEO by adding robots meta tag, sitemap link, and refining Open Graph/Twitter meta tags
    - Update canonical URL and social media meta tags to use absolute URLs
    - Replace team member Zelda Weaver with Amanda Strydom in site data
    - Clean up unused assets and add new fonts and team images

--- webpack.config.js
+++ webpack.config.js
@@ -26,24 +26,41 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ''
+                        }
+                    },
+                    'css-loader'
+                ]
             },
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ''
+                        }
+                    },
                     'css-loader',
                     'sass-loader'
                 ]
             },
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10000
+                    }
+                }
             },
             {
                 test: /\.(ttf|eot|svg|jpg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader"
+                type: 'asset/resource'
             }
         ]
     }
